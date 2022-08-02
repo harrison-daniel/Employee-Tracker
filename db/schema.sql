e997a3f67716484e93fce5c6fabbedfe7d5c7b97
Harrison Daniel <hd4n@outlook.com>
2022-08-02 15:12:18 -0400

    seeds.sql changes and DB seeded

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
@@ -7,26 +7,26 @@ CREATE TABLE department (
   name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
   department_id INTEGER,
-   FOREIGN KEY (department_id)
-   REFERENCES department(id)
-   ON DELETE SET NULL
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  roles_id INTEGER,
+  role_id INTEGER,
   manager_id INTEGER,
-    FOREIGN KEY (manager_id)
-    REFERENCES manager(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (roles_id)
-    REFERENCES roles(id)
-    ON DELETE SET NULL
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,30 +1,30 @@
 INSERT INTO department (name)
 VALUES
- ('HR'),
- ('Finance'),
- ('Customer Success'),
- ('Sales'),
- ('Marketing'),
- ('Development');
+('HR'),
+('Finance'),
+('Customer Success'),
+('Sales'),
+('Marketing'),
+('Development');
 
 
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES
- ('Employee Experience Manager', 70000, 1),
- ('Accounts Receivable Manager', 68000, 2),
- ('Customer Success Manager', 65000, 3),
- ('VP of Customer Success', 100000, 3),
- ('Account Executive', 68000, 4),
- ('VP of Sales', 100000, 4),
- ('Head of Marketing', 82000, 5),
- ('Development Manager', 86000, 6),
- ('Software Developer', 76000, 6);
+('Employee Experience Manager', 70000, 1),
+('Accounts Receivable Manager', 68000, 2),
+('Customer Success Manager', 65000, 3),
+('VP of Customer Success', 100000, 3),
+('Account Executive', 68000, 4),
+('VP of Sales', 100000, 4),
+('Head of Marketing', 82000, 5),
+('Development Manager', 86000, 6),
+('Software Developer', 76000, 6);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id)
 VALUES
- ('Michael', 'Allan', 3, 5),
- ('Seteven', 'Anderson', 3, 5),
- ('Victoria', 'Hodges', 3, 5),
- ('Rachel', 'Hill', 3, 5),
- ('Jack', 'Murray', 3, NULL),
- ('Joe', 'Robertson', 3, 5);
+('Michael', 'Allan', 1),
+('Seteven', 'Anderson', 2),
+('Victoria', 'Hodges', 3),
+('Rachel', 'Hill', 4),
+('Jack', 'Murray', 5),
+('Joe', 'Robertson', 6);
